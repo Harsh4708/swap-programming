570751007118211345cce6e8c2dc76a375d2b5f4
Harsh4708 <89592388+Harsh4708@users.noreply.github.com>
2021-08-27 20:55:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+
+var a = prompt('Enter the first variable: ');
+var b = prompt('Enter the second variable: ');
+function setup(){
+  var b2 = createButton("click here to Swap");
+  b2.mousePressed(swap)
+  }
+
+
+function swap()
+{
+ [ a,b ] = [b,a ];
+ console.log("The 1st number after swapping " + a );
+ console.log("The 2nd number after swapping " + a );
+}
